bcde0d7ec7dfdf2dbf8739a0238a7fc6e25b6d83
Darpannn <darpanapatel2003@gmail.com>
2026-09-23 23:21:00 +0530

    added service layer

---
+++ LibraryApi/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+using LibraryApi.DTOs;
+
+namespace LibraryApi.Services
+{
+    public interface IAuthService
+    {
+        Task<string?> RegisterAsync(RegisterDto dto);
+        Task<string?> LoginAsync(LoginDto dto);
+    }
+}
--- LibraryApi/obj/Debug/net10.0/LibraryApi.AssemblyInfo.cs
+++ LibraryApi/obj/Debug/net10.0/LibraryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f94345210dc74cca8bcf433f129f43a776d680d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022cd1083a72ecc4d137385222714b69417a0f5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
